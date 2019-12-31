f270412de77bc7ce2e14875642b9436ad2f00c48
Taylor Price <taylor@taylorbuiltsolutions.com>
2019-12-31 07:55:29 -0800

    Update CMakeLists.txt to find Boost. Inclue the header for Boost::ProgramOptions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <ryml.hpp>
 #include<iostream>
+ 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	std::cout << "Hello World!" << std::endl;
 }
