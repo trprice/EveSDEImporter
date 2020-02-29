13c25309c1b54a91fbd97e56be191e221b4cb0fd
Taylor Price <taylor@taylorbuiltsolutions.com>
2020-02-29 09:16:04 -0800

    Build and Logic Changes as follows:
    1. Set boost to use static libs because, for some reason, VS2019
    would throw linking errors w/ the dynamic libs saying it needed the
    static lib (then have multiply defined symbols if the static lib was
        present)
    
    2. Add logic to start parsing command line parameters.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,41 @@
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
+struct Options
+{
+	std::string sde_path;
+};
+
+void on_sde_path(std::string sde_path)
+{
+	std::cout << "Running with SDE data located at: " << sde_path << std::endl;
+}
+
+bool setup_options(int argc, char* argv[])
+{
+	po::options_description desc("Allowed Options");
+
+	Options opts;
+	desc.add_options()
+		("help", "Display help message")
+		("sde-path", po::value<std::string>(&opts.sde_path)->required()->notifier(&on_sde_path), "SDE Data File");
+	;
+	
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help"))
+	{
+		std::cout << desc << std::endl;
+		return false;
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	if (!setup_options(argc, argv))
+		return -1;
+
+	return 1;
 }
