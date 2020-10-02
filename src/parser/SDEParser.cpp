993613ed288f97773e1004eaeecb7be92e9fd96b
Taylor Price <taylor@taylorbuiltsolutions.com>
2020-10-02 16:11:18 -0700

    The file that I tested with had a newline at the end. This solution fixes it but presumes theres a newline. Perhaps I should check for it instead to be more flexible

--- src/parser/SDEParser.cpp
+++ src/parser/SDEParser.cpp
@@ -1,17 +1,22 @@
 #include "SDEParser.h"
 #include <fstream>
 #include <vector>
+#include <iostream>
 
 void SDEParser::readFile()
 {
 	std::ifstream ifs(this->fileName.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
 
-	std::ifstream::pos_type fileSize = ifs.tellg();
+	ifs.seekg(0, std::ios::end);
+	int length = ifs.tellg();
 	ifs.seekg(0, std::ios::beg);
 
-	std::vector<char> bytes(fileSize);
-	ifs.read(bytes.data(), fileSize);
+	std::vector<char> bytes(length);
+    ifs.read(bytes.data(), length);
 
-	c4::substr data(bytes.data(), fileSize);
+	c4::substr data(bytes.data(), bytes.size() - 1); // The file I tried with had a newline and caused ryml::parse to crash
+
+	std::cout << bytes.data() << std::endl;
+
 	parsedData = ryml::parse(data);
 }
