01b829ea254551d1a899a892f20b37587dbcf940
Taylor Price <trprice@gmail.com>
2021-07-18 21:52:31 -0700

    Add tests to the tests with fakes and remove CRLF

--- tests/parser_tests_with_fakes.cpp
+++ tests/parser_tests_with_fakes.cpp
@@ -1,17 +1,24 @@
 #include <catch2/catch.hpp>
-#include "fff.h"
 #include "../src/parser/SDEParser.h"
 #include <vector>
 #include <string>
 #include <iostream>
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VALUE_FUNC(c4::yml::Tree, parse, c4::substr);
 
 TEST_CASE("Try Parser With Fakes")
 {
-    SECTION("Test")
+    SECTION("Check call count of the fake")
     {
       std::string str = "-   agentType: NonAgent\n    agentTypeID: 1\n-   agentType: BasicAgent\n    agentTypeID: 2";
       std::vector<char> agentTypeData(str.begin(), str.end());
 
       SDEParser parser(agentTypeData);
+
+      REQUIRE(parse_fake.call_count == 0);
+      REQUIRE( 1 == 2 );
     }
 }
